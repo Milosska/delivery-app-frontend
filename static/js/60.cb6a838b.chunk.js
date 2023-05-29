c616ad7be7e430af455d9400813c09765fda8e0d
Milosska <110258611+Milosska@users.noreply.github.com>
2023-05-29 16:14:16 +0000

    Deploying to gh-pages from @ Milosska/delivery-app-frontend@3b836a6640a2799530032886f690ddca10e8e2bb 🚀

--- static/js/60.cb6a838b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdelivery_app_frontend=self.webpackChunkdelivery_app_frontend||[]).push([[60],{8060:function(e,n,r){r.r(n);var t=r(184);n.default=function(){return(0,t.jsx)(t.Fragment,{children:"ShoppingCartPage"})}}}]);
-//# sourceMappingURL=60.cb6a838b.chunk.js.map
